79c9e338122910c8a847683557b12dd03c6becf4
FuTuHengSheng <127578726+FuTuHengSheng@users.noreply.github.com>
2024-01-22 22:05:44 +0800

    Delete ATS-CTF/ATS-oncamous/2024-contest1/writeup directory

--- ATS-CTF/ATS-oncamous/2024-contest1/writeup/Ganlan/Reverse2/key.py
+++
@@ -1,8 +0,0 @@
-chip = 'WQZT`blf\\UZgV\\hfXXVhhWfQQb\\[kRoT\\[ZXP\\iUV\\hVXkVi~'
-flag = ''
-
-for char in chip:
-    original_char = chr((ord(char) ^ 0xE) + 13)
-    flag += original_char
-
-print(flag)
